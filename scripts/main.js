a5bb7b617c5bc63290d1463273d8c71dbf85ac1d
Justin L. Lattimore <jlattimore3d@gmail.com>
2017-05-29 22:29:44 -0400

    completed checkpoint-6-displayLiveScore

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,41 @@
-var animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
-  function(callback) { window.setTimeout(callback, 1000/60) };
+
+var animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) { window.setTimeout(callback, 1000/60) };
 
 var canvas = document.getElementById('mainCanvas');
-var width = 1200; 
-var height = 650;
+var width = 1200;
+var height = 625;
 canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
 
-window.onload = function(){
-    document.body.appendChild(canvas);
-    animate(step);
-}
-
-var step = function(){
-    update();
-    render();
-    animate(step);
-};
-
 var update = function(){
     player.update();
     ball.update(player.paddle, computer.paddle);
-    context.clearRect(0, 0, canvas.width, canvas.height)
+    score.update();
 };
 
-//makes a paddle
-function Paddle(x, y, width, height){
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    //this.fillStyle = colorInner;
-    //this.fill();    //added
-    //this.strokeStyle = colorOuter;
-    //this.stroke();  //added
-    this.x_speed = 0;
-    this.y_speed = 0;
+window.onload = function() {
+  document.body.appendChild(canvas);
+  animate(step);
+};
+
+var step = function() {
+  update();
+  render();
+  animate(step);
+};
+
+function Paddle(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.x_speed = 0;
+  this.y_speed = 0;
 }
 
-Paddle.prototype.render = function(){
-  context.fillStyle = "blue";
-  context.strokeStyle = "green";
+Paddle.prototype.render = function() {
+  context.fillStyle = "#0000FF";
   context.fillRect(this.x, this.y, this.width, this.height);
 };
 
@@ -53,78 +47,46 @@ function Player(){
 function Computer(){
     this.paddle = new Paddle(1150, 250, 15, 90);
 }
-Player.prototype.render = function(){
-    this.paddle.render();
-};
-Computer.prototype.render = function(){
-    this.paddle.render();
+
+Player.prototype.render = function() {
+  this.paddle.render();
 };
 
-//makes a Ball
+Computer.prototype.render = function() {
+  this.paddle.render();
+};
+
 function Ball(x, y){
     this.x = x;
     this.y = y;
-    this.x_speed = 3;
+    this.x_speed = 6;
     this.y_speed = 0;
-    this.radius = 5;
+    this.radius = 7;
 }
 
-Ball.prototype.render = function(){
-    context.beginPath();
-    context.arc(600, 300, 8, 0, 2 * Math.PI, false);
-    context.fillStyle = '#990099';
-    context.fill();
-};
-
-//animations (top is right, bottom is left)
-Ball.prototype.update = function(paddle1, paddle2){
-    this.x += this.x_speed;
-    this.y += this.y_speed;
-    var right_x = this.x - 5;
-    var right_y = this.y - 5;
-    var left_x = this.x + 5;
-    var left_y = this.y + 5;
-    
-    if(this.y - 5 < 0){ //hitting bottom wall ("5" the buffer?)
-        this.y = 5;
-        this.y_speed = -this.y_speed;
-    }else if(this.y + 5 > 650){ //hitting top wall ("5" the buffer?)
-        this.y = 645;
-        this.y_speed = -this.y_speed;
-    }
-    //a point is being scored
-    if(this.x < 0 || this.x > 1200){
-        this.x_speed = 3;
-        this.y_speed = 0;
-        this.x = 300;
-        this.y = 200;
-    }
-    //bouncing off paddles
-    if(right_x > 600){
-        if(right_x < (paddle1.x + paddle1.width) && left_y > paddle1.y && right_x < (paddle1.x + paddle1.width) && left_x > paddle1.x){
-            //player's paddle
-            this.x_speed = -3;
-            this.y_speed += (paddle1.y_speed / 2);
-            this.x += this.x_speed;
-        }
-    }else{
-        if(right_x < (paddle2.y + paddle2.width) && left_y > paddle2.y && right_x < (paddle2.x + paddle2.width) && left_x > paddle2.x){
-            //computer's paddle
-            this.x_speed = 3;
-            this.y_speed += (paddle2.y_speed / 2);
-            this.x += this.x_speed;
-        }
-    }
+Ball.prototype.render = function() {
+  context.beginPath();
+  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  context.fillStyle = "purple";
+  context.fill();
 };
 
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(200, 300);
+var ball = new Ball(600, 317);
+var score = new Score();
 
-var render = function(){
-    player.render();
-    computer.render();
-    ball.render();
+var render = function() {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, width, height);
+  player.render();
+  computer.render();
+  ball.render();
+};
+
+Ball.prototype.update = function() {
+  this.x += this.x_speed;
+  this.y += this.y_speed;
 };
 
 //setting keys up
@@ -169,49 +131,81 @@ Paddle.prototype.move = function(x, y){
     }
 }
 
-/* The non-tut version
-//This is supposed to make a triangle
-var canvas = document.getElementById('mainCanvas');
 
-var player = canvas.getContext('2d');
-player.beginPath();
-player.rect(10, 20, 5, 25);
-player.fillStyle = '#00cc99';
-player.fill();
-player.lineWidth = 1;
-player.strokeStyle = 'blue';
-player.stroke();
-//
-player.prototype.render
-
-var computer = canvas.getContext('2d');
-
-computer.beginPath();
-computer.rect(285, 20, 5, 25);
-computer.fillStyle = 'orange';
-computer.fill();
-computer.lineWidth = 1;
-computer.strokeStyle = 'red';
-computer.stroke();
-//
-computer.prototype.render
-
-var ball = canvas.getContext('2d');
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
-//var centerX = canvas.width / 2;
-//var centerY = canvas.height / 2;
-var radius = 5;
-
-ball.beginPath();
-ball.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-ball.fillStyle = '#990099';
-ball.fill();
-ball.lineWidth = 1;
-ball.strokeStyle = '#4d004d';
-ball.stroke();
-
-window.onload = function() {
-  render();
+//animations (top is right, bottom is left)
+Ball.prototype.update = function(paddle1, paddle2){
+    this.x -= this.x_speed;
+    this.y += this.y_speed;
+    var right_x = this.x - 5;
+    var right_y = this.y - 5;
+    var left_x = this.x + 5;
+    var left_y = this.y + 5;
+    
+    if(this.y - 5 < 0){ //hitting bottom wall ("5" the buffer?)
+        this.y = 5;
+        this.y_speed = -this.y_speed;
+    }else if(this.y + 5 > 650){ //hitting top wall ("5" the buffer?)
+        this.y = 645;
+        this.y_speed = -this.y_speed;
+    }
+    //a point is being scored
+    if(this.x < 0 || this.x > 1200){
+        this.x_speed = 6;
+        this.y_speed = 0;
+        this.x = 600;
+        this.y = 300;
+    }
+    //bouncing off paddles
+    if(right_x > 600){
+        if(right_x < (paddle1.x + paddle1.height) && left_y > paddle1.y && right_x < (paddle1.x + paddle1.width) && left_x > paddle1.x){
+            //player's paddle
+            //this.x_speed = -6;
+            this.x_speed = 6;
+            this.y_speed += (paddle1.y_speed / 2);
+            this.x += this.x_speed;
+        }
+    }else{
+        if(right_x < (paddle2.y + paddle2.height) && left_y > paddle2.y && right_x < (paddle2.x + paddle2.width) && left_x > paddle2.x){
+            //computer's paddle
+            //this.x_speed = 6;
+            this.x_speed = -6;
+            this.y_speed += (paddle2.y_speed / 2);
+            this.x += this.x_speed;
+        }
+    }
 };
+
+//Score functions
+function Score(){
+    this.pScore = 0;
+    this.cScore = 0;
+}
+
+Score.prototype.update = function(){
+    if(ball.x > 1199){
+        this.pScore++;
+        console.log(this.pScore)
+        document.getElementsByClassName('score-keep')[0].getElementsByTagName('h5')[0].innerHTML = this.pScore;
+        if(this.pScore <= 10){
+            //player victory is going to go here
+        }
+    }
+    if(ball.x < 1){
+        this.cScore++;
+        console.log(this.cScore)
+        document.getElementsByClassName('score-keep')[0].getElementsByTagName('h5')[1].innerHTML = this.cScore;
+        if(this.cScore <= 10){
+            //computerWon();
+        }
+    }
+};
+
+/*
+var scoreKeep = function(){
+    //update player score
+    document.getElementsByClassName('score-keep')[0].getElementsByTagName('h5')[0].innerHTML = pScore;
+
+    //update computer score
+    document.getElementsByClassName('score-keep')[0].getElementsByTagName('h5')[1].innerHTML = cScore;
+}
 */
